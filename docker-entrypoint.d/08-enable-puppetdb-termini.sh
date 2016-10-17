2234f7124f8f7d6257268974b38deb5d7cff5d26
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2016-10-17 17:30:32 +0100

    Updated some init scripts

--- docker-entrypoint.d/08-enable-puppetdb-termini.sh
+++ docker-entrypoint.d/08-enable-puppetdb-termini.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if test -n "${CA}" && ! ${CA} && getent hosts puppetdb > /dev/null 2>&1 ; then
+if test -n "${CA}" && ! ${CA} && getent hosts ${DB_SERVER} > /dev/null 2>&1 ; then
   echo "Configure puppetdb-termini"
   puppet config set storeconfigs true --section master
   puppet config set storeconfigs_backend puppetdb --section master
@@ -10,5 +10,10 @@ master:
   facts:
     terminus: puppetdb
     cache: yaml
+EOF
+  cat << EOF > /etc/puppetlabs/puppet/puppetdb.conf
+[main]
+server_urls = https://${DB_SERVER}:8081/
+soft_write_failure = true
 EOF
 fi
--- docker-entrypoint.d/10-set-ca_server.sh
+++ docker-entrypoint.d/10-set-ca_server.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 if test -n "${CA}" && ! $CA; then
+  test $CA_SERVER
   puppet config set ca_server $CA_SERVER --section main
   certname=$(puppet config print certname)
   sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/services.d/ca.cfg
@@ -15,6 +16,6 @@ set \$sslcacert/@value '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
 defnode sslcrlpath \$webserver/@simple[.='ssl-crl-path'] ssl-crl-path
 set \$sslcrlpath/@value '/etc/puppetlabs/puppet/ssl/crl.pem'
 EOF
-  puppet agent -t > /dev/null 2>&1
+  puppet agent -t > /dev/null 2>&1 || /bin/true
   puppet agent -t > /dev/null 2>&1
 fi
